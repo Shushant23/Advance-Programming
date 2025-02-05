bcddd7066e8b4ebc50cb79396cbeaa64fcb7ca84
Shushant singh <167003930+Shushant23@users.noreply.github.com>
2025-02-05 12:02:21 +0530

    Update Pallindrome.cpp

--- Pallindrome.cpp
+++ Pallindrome.cpp
@@ -1,14 +1,25 @@
 class Solution {
-public:
-    bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-        
-        return x == reversedHalf || x == reversedHalf / 10;
+  public:
+    bool isPalindrome(int n) {
+    if (n < 0) return false;  
+    if (n == 0) return true;  
+
+    vector<int> ans;
+    while (n) {
+        int rem = n % 10;
+        ans.push_back(rem);
+        n = n / 10;
     }
+    
+    int start = 0, end = ans.size() - 1;
+    while (start < end) {
+        if (ans[start] != ans[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true; 
+
 };
