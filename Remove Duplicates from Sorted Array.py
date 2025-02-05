4d4a50051f934f5cf2cd8be7fb13391526fff02d
Shushant singh <167003930+Shushant23@users.noreply.github.com>
2025-02-05 11:40:14 +0530

    Update and rename Remove Duplicates from Sorted Array.py to Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        uniqueElements = sorted(set(nums)) 
-        k = len(uniqueElements)
-        for i in range(k):  
-            nums[i] = uniqueElements[i]
-        return k
